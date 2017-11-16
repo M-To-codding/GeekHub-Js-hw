f29c2cff39329ac34831804ad0cd583a09d9f3cc
= <=>
2017-11-16 17:56:03 +0200

    Added main object

---
+++ HomeWork6/toDoList.js
@@ -0,0 +1,19 @@
+var taskObj = {
+
+    setId: function (id) {
+        this.id = id;
+    },
+
+    getId: function () {
+        return this.id;
+    },
+
+    setStatus: function (status) {
+        this.status = status;
+    },
+
+    getStatus: function () {
+        return this.status;
+    }
+};
+
