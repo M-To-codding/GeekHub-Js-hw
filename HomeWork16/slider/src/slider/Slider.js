73fad90a0726d28b7e7c29afcdcc51385600a2ea
im <maryna.todorishyna@ekreative.com>
2018-02-08 00:18:45 +0200

    Add class for slider

---
+++ HomeWork16/slider/src/slider/Slider.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Slider extends Component {
+
+  constructor(props, slideSpeed='2000') {
+    super(props);
+
+    this.slideSpeed = slideSpeed;
+  }
+
+  render() {
+    return (
+      <div className="content">
+
+      </div>
+    );
+  }
+}
+
+export default Slider;
