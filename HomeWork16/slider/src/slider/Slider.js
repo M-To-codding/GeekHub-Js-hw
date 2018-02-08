b4309ccd6d447f631f4b92056d42437cc2c4b424
im <maryna.todorishyna@ekreative.com>
2018-02-09 01:14:05 +0200

    Add opportunity for get slider options and its rendering

--- HomeWork16/slider/src/slider/Slider.js
+++ HomeWork16/slider/src/slider/Slider.js
@@ -1,19 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Slider extends Component {
 
-  constructor(props, slideSpeed='2000') {
+  constructor(props) {
     super(props);
+    this.items = props.items;
+    this.slideSpeed = props.slideSpeed;
+    this.allSlides = [];
+    console.log(this.allSlides);
+  }
 
-    this.slideSpeed = slideSpeed;
+   handlePrev() {
+    let firstSlide = this.allSlides[0];
+    this.allSlides.splice(firstSlide, 1);
+    this.allSlides.push(firstSlide);
+    console.log(this.allSlides);
+  }
+
+   handleNext() {
+    let lastSlide = this.allSlides[this.allSlides.length-1];
+    this.allSlides.splice(lastSlide, 1);
+    this.allSlides.unshift(lastSlide);
+    console.log(this.allSlides);
   }
 
   render() {
-    return (
-      <div className="content">
 
+    const slides = this.items.map((slide, index) =>
+      <li key={index} className="slide-item">
+        <img src={slide} alt=""/>
+      </li>
+    )
+
+    return (
+      <div className="slider">
+        <div className="slide-prev" onClick={this.handlePrev}> </div>
+        <div className="slide-next" onClick={this.handleNext}> </div>
+        <ul className="slides-list">
+          {slides}
+        </ul>
       </div>
+
     );
+
   }
 }
 
