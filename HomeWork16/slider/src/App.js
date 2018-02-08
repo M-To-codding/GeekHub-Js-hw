248b347bb639ffaa78152f566b1b4af31eb2ae93
im <maryna.todorishyna@ekreative.com>
2018-02-09 01:16:02 +0200

    Create and render sliders

--- HomeWork16/slider/src/App.js
+++ HomeWork16/slider/src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Slider from './slider/Slider'
+import data from './data/options.json';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="Slider">
+  constructor(props){
+    super(props);
 
+    this.slider = new Slider(data.slider1);
+    this.slider2 = new Slider(data.slider2);
+  }
+
+  render() {
+
+    return (
+      <div className="content">
+        {this.slider.render()}
+        {this.slider2.render()}
       </div>
     );
   }
