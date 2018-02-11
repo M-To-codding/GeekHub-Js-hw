94bb0d39546a754806f30316989059d561d0aaca
im <maryna.todorishyna@ekreative.com>
2018-02-12 00:12:44 +0200

    Simplify class Slider

--- HomeWork16/slider/src/slider/Slider.js
+++ HomeWork16/slider/src/slider/Slider.js
@@ -6,41 +6,37 @@ class Slider extends Component {
     super(props);
     this.items = props.items;
     this.slideSpeed = props.slideSpeed;
-    this.allSlides = [];
-    console.log(this.allSlides);
+
+    console.log(this.items.indexOf(this.items[0]));
   }
 
-   handlePrev() {
-    let firstSlide = this.allSlides[0];
-    this.allSlides.splice(firstSlide, 1);
-    this.allSlides.push(firstSlide);
-    console.log(this.allSlides);
+  changeSlides() {
+    let firstItem = this.items.shift();
+    this.items.push(firstItem);
+    console.log('ldsdk');
   }
 
-   handleNext() {
-    let lastSlide = this.allSlides[this.allSlides.length-1];
-    this.allSlides.splice(lastSlide, 1);
-    this.allSlides.unshift(lastSlide);
-    console.log(this.allSlides);
+  componentDidMount() {
+     setInterval(
+      () => this.changeSlides(),
+      this.slideSpeed
+    );
   }
+  //
+  // componentWillUnmount() {
+  //   clearInterval(this.slideInterval);
+  // }
 
   render() {
 
-    const slides = this.items.map((slide, index) =>
-      <li key={index} className="slide-item">
-        <img src={slide} alt=""/>
-      </li>
-    )
-
     return (
       <div className="slider">
-        <div className="slide-prev" onClick={this.handlePrev}> </div>
-        <div className="slide-next" onClick={this.handleNext}> </div>
         <ul className="slides-list">
-          {slides}
+          <li key={'slide'} className="slide-item">
+            <img src={this.items[0]} alt=""/>
+          </li>
         </ul>
       </div>
-
     );
 
   }
